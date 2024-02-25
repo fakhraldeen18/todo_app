48a67e608dd2c36f2da14a017fd744699224dd9d
fakhraldeen18 <fakhraldeen18@gmail.com>
2024-02-25 14:42:35 +0300

    Added an adding feature

--- index.js
+++ index.js
@@ -20,3 +20,28 @@ function renderTodo(array) {
   });
 }
 renderTodo(todolist);
+
+// Adding a Todo
+function addToDos(input) {
+    if(input.value === ""){
+        alert("Please enter a todo");
+    }
+    else{
+  const li = document.createElement("li");
+  li.classList.add("todo-item");
+  li.textContent = input.value;
+  ul.appendChild(li);
+  input.value = "";
+  // ---------------------
+  const deleteButton = document.createElement("span");
+  deleteButton.classList.add("delete-button");
+  deleteButton.textContent = "\u00D7";
+  li.appendChild(deleteButton);
+    }
+}
+const addTodoBtn = document.querySelector(".todo-btn");
+addTodoBtn.addEventListener("click", (e) => {
+  let input = document.querySelector(".todo-input");
+  addToDos(input);
+  e.preventDefault();
+});
